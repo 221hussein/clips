315607f1b7d71dbc93e3d5d6de6357ede1d285c7
221hussein <yuzinpream51@gmail.com>
2024-08-06 14:10:34 +0000

    toggle modal

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../../service/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+  // providers: [ModalService]
+})
+export class ModalComponent implements OnInit {
+
+  constructor(public modal : ModalService) {
+      
+  }
+
+  ngOnInit(): void {
+      
+  }
+}
